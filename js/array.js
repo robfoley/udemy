96264cdcdbc1e4736a6e807afb4bcc72ec5ddc50
Rob <rob.foley0802@gmail.com>
2015-10-27 16:31:53 -0500

    Continue progress
    Continue progress

---
+++ js/array.js
@@ -0,0 +1,107 @@
+// Function Definitions
+function outputArray(theArray)
+{
+    for(var i = 0; i < placesToVisit.length; i++)
+    {
+        document.write(placesToVisit[i] + "<br/>");
+    }
+}
+
+function displayPlayerScore(theScore)
+{
+    //playerScore = 1000;
+    theScore += 1000;
+    document.write("Your score is:  " + theScore);
+}
+
+function addTheseNumbers(numOne, numTwo)
+{
+    //var total = numOne + numTwo;
+    //document.write(total);
+    return(numOne + numTwo);
+}
+
+function english()
+{
+    alert("English = Hello! How are you?");
+}
+
+function spanish()
+{
+    alert("Spanish = Hola. Como estas?");
+}
+
+function hebrew()
+{
+    alert("Hebrew = Shalom!");
+}
+
+function french()
+{
+    alert("Bon Jour!");
+}
+
+function add(x, y)
+{
+    document.write(x + " + " + y + " = ");
+    document.write(x + y);
+}
+
+function subtract(x, y)
+{
+    if(x > y)
+        {
+            document.write(x + " - " + y + " = ");
+            document.write(x - y);
+        }
+    else
+        {
+            document.write(y + " - " + x + " = ");
+            document.write(y - x);
+        }
+}
+
+function multiply(x, y)
+{
+    document.write(x + " * " + y + " = ");
+    document.write(x * y);
+}
+
+function divide(x, y)
+{
+    document.write(x + " / " + y + " = ");
+    var ans = parseInt(x / y);
+    document.write(ans);
+}
+
+function mathProb(x, y, z)
+{
+    if(z == "+")
+    {
+        document.write(x + " + " + y + " = ");
+        document.write(x + y);
+    }
+    else if(z == "-")
+    {
+        if(x > y)
+        {
+            document.write(x + " - " + y + " = ");
+            document.write(x - y);
+        }
+        else
+        {
+            document.write(y + " - " + x + " = ");
+            document.write(y - x);
+        }
+    }
+    else if(z == "*")
+    {
+        document.write(x + " * " + y + " = ");
+        document.write(x * y);         
+    }
+    else if(z == "/")
+    {
+        document.write(x + " / " + y + " = ");
+        document.write(x / y);         
+    }
+}
